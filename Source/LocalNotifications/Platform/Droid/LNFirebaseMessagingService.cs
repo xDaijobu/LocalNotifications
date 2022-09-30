dd545de6e215e13496da2838ff92cddc003cd689
Cristover Wurangian <christwurangian@gmail.com>
2022-09-30 10:06:19 +0700

    'LinkerSafeAttribute' is obsolete: 'For .NET 6+, please use: [assembly: global::System.Reflection.AssemblyMetadata("IsTrimmable", "True")]'

--- Source/LocalNotifications/Platform/Droid/LNFirebaseMessagingService.cs
+++ Source/LocalNotifications/Platform/Droid/LNFirebaseMessagingService.cs
@@ -1,4 +1,4 @@
-﻿#if MONOANDROID10_0
+﻿#if MONOANDROID
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Source/LocalNotifications/Platform/Droid/ManifestInfo.cs
+++ Source/LocalNotifications/Platform/Droid/ManifestInfo.cs
@@ -2,4 +2,8 @@
 using Android.App;
 
 [assembly: UsesPermission(Manifest.Permission.ReceiveBootCompleted)]
+#if NET6_0_OR_GREATER
+[assembly: global::System.Reflection.AssemblyMetadata("IsTrimmable", "True")]
+#else
 [assembly: LinkerSafe]
+#endif
