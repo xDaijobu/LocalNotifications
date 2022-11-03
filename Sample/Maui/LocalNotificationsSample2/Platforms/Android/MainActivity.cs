a71aba51a925bd83240bc0782a1eb3c46d4b3030
Cristover Wurangian <christwurangian@gmail.com>
2022-11-03 11:11:18 +0700

    perubahan terakhir yg ada di SVN (r48398)

--- Sample/Maui/LocalNotificationsSample2/Platforms/Android/MainActivity.cs
+++ Sample/Maui/LocalNotificationsSample2/Platforms/Android/MainActivity.cs
@@ -9,18 +9,5 @@ namespace LocalNotificationsSample2;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-    protected override void OnCreate(Bundle savedInstanceState)
-    {
-        base.OnCreate(savedInstanceState);
-
-        LocalNotifications.Platform.Droid.NotificationService.NotificationTapped(Intent);
-    }
-
-    protected override void OnNewIntent(Intent intent)
-    {
-        base.OnNewIntent(intent);
-
-        LocalNotifications.Platform.Droid.NotificationService.NotificationTapped(intent);
-    }
 }
 
