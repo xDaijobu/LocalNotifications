c91f3a65fee4af956a5af090e5b874bb0584e49a
Cristover Wurangian <christwurangian@gmail.com>
2022-11-08 15:30:37 +0700

    Initialize ny makin simple

---
+++ Source/LocalNotifications/AppBuilderExtensions.net.cs
@@ -0,0 +1,46 @@
+﻿#if NET6_0
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.LifecycleEvents;
+
+namespace LocalNotifications;
+
+public static class AppBuilderExtensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <param name="isFirebase"></param>
+    /// <param name="autoRegistration">Auto registers for push notifications if second parameter (a.k.a isFirebase) is true</param>
+    /// <returns></returns>
+    public static MauiAppBuilder UseLocalNotifications(this MauiAppBuilder builder, bool isFirebase, bool autoRegistration)
+    {
+        builder// https://learn.microsoft.com/en-us/dotnet/maui/fundamentals/app-lifecycle
+              .ConfigureLifecycleEvents(events =>
+              {
+        #if ANDROID
+	        events.AddAndroid(android => android
+		        .OnCreate((activity, bundle) => OnNotificationTapped(activity.Intent))
+		        .OnNewIntent((activity, intent) => OnNotificationTapped(intent)));
+
+                static void OnNotificationTapped(Android.Content.Intent intent)
+	            {
+                    Platform.Droid.NotificationService.NotificationTapped(intent);
+                }
+        #elif IOS
+	            events.AddiOS(iOS => iOS.FinishedLaunching((app, options) => InitLocalNotifications(options, isFirebase, autoRegistration)));
+
+	            static bool InitLocalNotifications(Foundation.NSDictionary options, bool isFirebase, bool autoRegistration)
+	            {
+                    Platform.iOS.NotificationService.Initialize(options: options,
+                                                                isFirebase: isFirebase,
+                                                                autoRegistration: autoRegistration);
+		            return true;
+                }
+        #endif
+              });
+
+        return builder;
+    }
+}
+#endif
