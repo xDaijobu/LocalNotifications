a6270263abf45534af5f43d987802ab116456705
Cristover Wurangian <christwurangian@gmail.com>
2022-11-10 10:43:13 +0700

    Update ManifestInfo.cs
    Android 12

--- Source/LocalNotifications/Platform/Droid/ManifestInfo.cs
+++ Source/LocalNotifications/Platform/Droid/ManifestInfo.cs
@@ -2,6 +2,11 @@
 using Android.App;
 
 [assembly: UsesPermission(Manifest.Permission.ReceiveBootCompleted)]
+
+#if ANDROID31_0_OR_GREATER
+[assembly: UsesPermission(Manifest.Permission.ScheduleExactAlarm)]
+#endif
+
 #if NET6_0_OR_GREATER
 [assembly: global::System.Reflection.AssemblyMetadata("IsTrimmable", "True")]
 #else
